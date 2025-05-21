25bea1be8872ab44f28608879eaeb7214d1d98f8
Kaique Reis <kreis1265@gmail.com>
2025-05-21 03:17:57 -0300

    feat: add method to create a new UrlClick entry

--- UrlShortener.Domain/Entities/UrlClick.cs
+++ UrlShortener.Domain/Entities/UrlClick.cs
@@ -2,10 +2,25 @@ namespace UrlShortener.Domain.Entities;
 
 public class UrlClick
 {
-    public DateTime ClickedAt { get; set; } = DateTime.Now;
+    public DateTime ClickedAt { get; set; }
     public string IpAddress { get; set; } = string.Empty;
     public string? Referrer { get; set; }
     public string? UserAgent { get; set; }
     public string? CountryCode { get; set; }
     public string? DeviceType { get; set; }
+
+    public ShortUrl? ShortUrl { get; set; }
+    public Guid ShortUrlId { get; set; }
+
+    public static UrlClick Create(ShortUrl shortUrl, string ipAddress, string? referrer, string? userAgent)
+    {
+        return new UrlClick
+        {
+            ShortUrlId = shortUrl.Id,
+            ClickedAt = DateTime.Now,
+            IpAddress = ipAddress,
+            Referrer = referrer,
+            UserAgent = userAgent
+        };
+    }
 }
