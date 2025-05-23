cef5f5f455ee1535a1adcd13cf1e252192f4254a
Kaique Reis <kreis1265@gmail.com>
2025-05-23 15:15:20 -0300

    feat: add Scalar for endpoint visualization

--- UrlShortener.Api/Program.cs
+++ UrlShortener.Api/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +12,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(opt =>
+    {
+        opt.WithTheme(ScalarTheme.DeepSpace)
+            .WithBaseServerUrl("/api-dev");
+    });
 }
 
 app.UseHttpsRedirection();
