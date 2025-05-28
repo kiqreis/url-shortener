3fce8894e001f074f7c898921f7fd206df0ec8e4
Kaique Reis <kreis1265@gmail.com>
2025-05-28 02:41:15 -0300

    feat: create ShortenUrlRequest for incoming shorten URL requests

---
+++ UrlShortener.Application/UrlShortening/DTOs/Requests/ShortenUrlRequest.cs
@@ -0,0 +1,9 @@
+﻿using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Application.UrlShortening.DTOs.Requests;
+
+public record ShortenUrlRequest(string OriginalUrl, Guid UserId, string? CustomCode = null, TimeSpan? Duration = null)
+{
+    public static implicit operator ShortUrl(ShortenUrlRequest request) =>
+        ShortUrl.Create(request.OriginalUrl, request.CustomCode ?? string.Empty, request.UserId, request.Duration);
+}
