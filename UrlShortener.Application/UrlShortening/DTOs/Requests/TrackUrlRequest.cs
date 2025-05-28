f94c84b993e7ba743358755e1f1b175d2299fb67
Kaique Reis <kreis1265@gmail.com>
2025-05-28 02:43:02 -0300

    feat: implement TrackUrlRequest for tracking URL access

---
+++ UrlShortener.Application/UrlShortening/DTOs/Requests/TrackUrlRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace UrlShortener.Application.UrlShortening.DTOs.Requests;
+
+public record TrackUrlRequest(string ShortCode, string IpAddress, string? Referrer = null, string? UserAgent = null);
