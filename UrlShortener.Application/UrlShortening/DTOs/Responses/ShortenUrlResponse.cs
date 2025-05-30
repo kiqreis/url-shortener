f3285972539353f0041a6b83b425f1790959b992
Kaique Reis <kreis1265@gmail.com>
2025-05-30 13:47:12 -0300

    feat: add dto for URL shortening response

---
+++ UrlShortener.Application/UrlShortening/DTOs/Responses/ShortenUrlResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace UrlShortener.Application.UrlShortening.DTOs.Responses;
+
+public record ShortenUrlResponse(string OriginalUrl, string ShortUrl, string ShortCode, DateTime CreatedAt, DateTime? ExpiresAt);
