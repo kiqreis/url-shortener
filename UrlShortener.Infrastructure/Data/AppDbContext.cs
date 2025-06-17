23c303132f525efbe6299afc80efe8e72561285e
Kaique Reis <kreis1265@gmail.com>
2025-06-17 18:03:13 -0300

    feat: configure database mapping for ApplicationUser entity

---
+++ UrlShortener.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Infrastructure.Identity;
+
+namespace UrlShortener.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid, IdentityUserClaim<Guid>, IdentityUserRole<Guid>,
+        IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ UrlShortener.Infrastructure/Mappings/ApplicationUserMapper.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UrlShortener.Infrastructure.Identity;
+
+namespace UrlShortener.Infrastructure.Mappings;
+
+public class ApplicationUserMapper : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.ToTable("IdentityUser");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .UseIdentityColumn();
+
+        builder.HasIndex(x => x.NormalizedUserName).IsUnique();
+        builder.HasIndex(x => x.NormalizedEmail).IsUnique();
+
+        builder.Property(x => x.Email)
+            .HasMaxLength(180)
+            .IsRequired();
+
+        builder.Property(x => x.NormalizedEmail)
+            .HasMaxLength(180);
+
+        builder.Property(x => x.UserName)
+            .HasMaxLength(180);
+
+        builder.Property(x => x.NormalizedUserName)
+            .HasMaxLength(180);
+
+        builder.Property(x => x.PhoneNumber)
+            .HasMaxLength(20);
+
+        builder.Property(x => x.ConcurrencyStamp).IsConcurrencyToken();
+
+        builder.HasMany<IdentityUserClaim<Guid>>()
+            .WithOne()
+            .HasForeignKey(uc => uc.UserId)
+            .IsRequired();
+
+        builder.HasMany<IdentityUserLogin<Guid>>()
+            .WithOne()
+            .HasForeignKey(ul => ul.UserId)
+            .IsRequired();
+
+        builder.HasMany<IdentityUserToken<Guid>>()
+            .WithOne()
+            .HasForeignKey(ut => ut.UserId)
+            .IsRequired();
+
+        builder.HasMany<IdentityUserRole<Guid>>()
+            .WithOne()
+            .HasForeignKey(ur => ur.UserId)
+            .IsRequired();
+    }
+}
