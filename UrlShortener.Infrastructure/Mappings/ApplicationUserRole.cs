36b16187a98f08018894ebb97e96bc2d1ee216dd
Kaique Reis <kreis1265@gmail.com>
2025-06-17 18:10:07 -0300

    feat: configure database mapping for ApplicationUserRole

---
+++ UrlShortener.Infrastructure/Mappings/ApplicationUserRole.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace UrlShortener.Infrastructure.Mappings;
+
+public class ApplicationUserRole : IEntityTypeConfiguration<IdentityUserRole<Guid>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserRole<Guid>> builder)
+    {
+        builder.ToTable("ApplicationUserRole");
+
+        builder.HasKey(x => new
+        {
+            x.UserId,
+            x.RoleId
+        });
+    }
+}
