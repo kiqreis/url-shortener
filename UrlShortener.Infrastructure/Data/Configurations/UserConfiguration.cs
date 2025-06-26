86b01c62efc2ac5e2d0fb191121f9c5d95c5feb2
Kaique Reis <kreis1265@gmail.com>
2025-06-26 03:11:07 -0300

    feat: map User domain class to database

---
+++ UrlShortener.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Infrastructure.Data.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+        
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Id)
+            .HasColumnType("uniqueidentifier")
+            .IsRequired();
+
+        builder.Property(x => x.Email)
+            .HasColumnType("nvarchar")
+            .HasMaxLength(180)
+            .IsRequired();
+        
+        builder.Property(x => x.CreatedAt)
+            .HasColumnType("datetime")
+            .HasDefaultValueSql("getdate()");
+        
+        builder.Property(x => x.IsVerified)
+            .HasColumnType("bit")
+            .IsRequired();
+
+        builder.Property(x => x.Plan)
+            .HasConversion<string>()
+            .HasColumnType("nvarchar")
+            .HasMaxLength(20);
+        
+        throw new NotImplementedException();
+    }
+}
