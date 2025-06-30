b2c281b3d7c70e7e0a9cdd82388e0f556dc668a6
Kaique Reis <kreis1265@gmail.com>
2025-06-30 15:00:56 -0300

    feat: implement JwtTokenValidation to handle JWT validation and structured result

---
+++ UrlShortener.Domain/Common/Security/JwtTokenValidation.cs
@@ -0,0 +1,28 @@
+using System.Security.Claims;
+
+namespace UrlShortener.Domain.Common.Security;
+
+public class JwtTokenValidation
+{
+    public bool IsValid { get; set; }
+    public ClaimsPrincipal? Principal { get; set; }
+    public string? ErrorMessage { get; set; }
+    public Dictionary<string, string> Claims { get; set; } = new();
+    public string? Subject { get; set; }
+
+    public static JwtTokenValidation Success(ClaimsPrincipal principal, string subject,
+        Dictionary<string, string> claims)
+        => new()
+        {
+            IsValid = true,
+            Principal = principal,
+            Subject = subject,
+            Claims = claims,
+        };
+
+    public static JwtTokenValidation Failure(ClaimsPrincipal principal, string message) => new()
+    {
+        IsValid = false,
+        ErrorMessage = message,
+    };
+}
