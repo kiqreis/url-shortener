e38d2aa819cff69e3f0f280f8319f4facc8f0341
Kaique Reis <kreis1265@gmail.com>
2025-07-07 21:22:23 -0300

    refactor: clean up user registration method to enhance clarity and separation of concerns

---
+++ UrlShortener.Infrastructure/Identity/Handlers/RegisterUserHandler.cs
@@ -0,0 +1,17 @@
+using UrlShortener.Application.Users.DTOs.Requests;
+using UrlShortener.Domain.Entities;
+using UrlShortener.Domain.Repositories;
+
+namespace UrlShortener.Infrastructure.Identity.Handlers;
+
+public class RegisterUserHandler(IUserRepository userRepository)
+{
+    public async Task<User> CreateUser(ApplicationUser applicationUser, CreateUserRequest request)
+    {
+        var user = User.Create(applicationUser.Id, request.Email);
+        
+        await userRepository.AddAsync(user);
+
+        return user;
+    }
+}
