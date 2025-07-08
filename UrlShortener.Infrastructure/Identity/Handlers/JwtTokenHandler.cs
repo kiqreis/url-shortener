bd0f05eefe6919ffcb8b8811f4c3d3b49906d4c6
Kaique Reis <kreis1265@gmail.com>
2025-07-07 21:20:29 -0300

    refactor: restructure JWT generation for better readability and maintainability

---
+++ UrlShortener.Infrastructure/Identity/Handlers/JwtTokenHandler.cs
@@ -0,0 +1,24 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using UrlShortener.Domain.Common.Security;
+
+namespace UrlShortener.Infrastructure.Identity.Handlers;
+
+public class JwtTokenHandler(UserManager<ApplicationUser> userManager, IJwtService jwtService, ILogger logger)
+{
+    public async Task<string> GenerateJwtTokenAsync(ApplicationUser user, Guid userId)
+    {
+        var roles = await userManager.GetRolesAsync(user);
+        var claims = new Dictionary<string, string>();
+
+        if (roles.Any())
+            claims.Add(ClaimTypes.Role, string.Join(",", roles));
+
+        var token = jwtService.GenerateToken(userId.ToString(), claims);
+
+        logger.LogInformation("JWT token generated successfully for user: {UserId}", userId);
+        
+        return token.Value;
+    }   
+}
