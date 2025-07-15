d454b07f99118f9414497685dc15814282c1b4d2
Kaique Reis <kreis1265@gmail.com>
2025-07-15 16:44:40 -0300

    feat: implement UserService to encapsulate user-related business logic

---
+++ src/Backend/UrlShortener.Application/Users/Services/UserService.cs
@@ -0,0 +1,9 @@
+﻿using UrlShortener.Domain.Entities;
+using UrlShortener.Domain.Repositories;
+
+namespace UrlShortener.Application.Users.Services;
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    public async Task<User?> GetByEmailAsync(string email) => await userRepository.GetByEmailAsync(email);
+}
