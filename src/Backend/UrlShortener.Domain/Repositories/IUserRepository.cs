f0e44f2cc05b006af646e89bd2b740773898e27a
Kaique Reis <kreis1265@gmail.com>
2025-07-15 16:28:10 -0300

    feat: add method to retrieve user by email in UserRepository

--- src/Backend/UrlShortener.Domain/Repositories/IUserRepository.cs
+++ src/Backend/UrlShortener.Domain/Repositories/IUserRepository.cs
@@ -5,4 +5,5 @@ namespace UrlShortener.Domain.Repositories;
 public interface IUserRepository
 {
     Task AddAsync(User user);
+    Task<User?> GetByEmailAsync(string email);
 }
--- src/Backend/UrlShortener.Infrastructure/Repositories/UserRepository.cs
+++ src/Backend/UrlShortener.Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using UrlShortener.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Domain.Entities;
 using UrlShortener.Domain.Repositories;
 using UrlShortener.Infrastructure.Data;
 
@@ -11,4 +12,7 @@ public class UserRepository(AppDbContext context) : IUserRepository
         await context.DomainUsers.AddAsync(user);
         await context.SaveChangesAsync();
     }
+
+    public async Task<User?> GetByEmailAsync(string email) => 
+        await context.DomainUsers.AsNoTracking().FirstOrDefaultAsync(user => user.Email.Equals(email));
 }
