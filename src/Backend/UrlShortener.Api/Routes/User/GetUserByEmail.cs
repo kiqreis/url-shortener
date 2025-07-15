6932d8ecd7a65dcbe3a4fc8de1960998b318d42b
Kaique Reis <kreis1265@gmail.com>
2025-07-15 16:48:17 -0300

    feat: add REST endpoint to retrieve user by email with response mapping

---
+++ src/Backend/UrlShortener.Api/Routes/User/GetUserByEmail.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Api.Common.Api;
+using UrlShortener.Application.Users.Services;
+
+namespace UrlShortener.Api.Routes.User;
+
+public class GetUserByEmail : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder routeBuilder) => routeBuilder.MapGet("/{email:string}", HandleAsync)
+        .WithName("User: get user")
+        .WithDescription("Get user by email")
+        .WithOrder(2)
+        .WithOpenApi();
+
+    private static async Task<IResult> HandleAsync([FromRoute] string email, [FromServices] IUserService handler)
+    {
+        var result = await handler.GetByEmailAsync(email);
+
+        return Results.Created($"{result!.Email}", result);
+    }
+}
