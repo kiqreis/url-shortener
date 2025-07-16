f97af7872515595411b8fffa4c548e97cee3fbf0
Kaique Reis <kreis1265@gmail.com>
2025-07-16 14:57:07 -0300

    feat: implement UserResponse DTO to standardize user data across multiple endpoints

---
+++ src/Backend/UrlShortener.Application/Users/DTOs/Responses/UserProfileResponse.cs
@@ -0,0 +1,10 @@
+﻿using UrlShortener.Domain.Entities;
+using UrlShortener.Domain.Enums;
+
+namespace UrlShortener.Application.Users.DTOs.Responses;
+
+public class UserProfileResponse
+{
+    public string Email { get; set; } = string.Empty;
+    public UserPlan Plan { get; set; } = UserPlan.Free;
+}
