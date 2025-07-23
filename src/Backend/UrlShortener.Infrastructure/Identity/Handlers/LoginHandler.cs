c45d26b08e41e45333029189420721dbeb433f39
Kaique Reis <kreis1265@gmail.com>
2025-07-23 15:04:46 -0300

    feat: implement ValidateCredentialsAsync in LoginHandler to handle user authentication logic with validation

---
+++ src/Backend/UrlShortener.Infrastructure/Identity/Handlers/LoginHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UrlShortener.Infrastructure.Identity.Handlers;
+
+public class LoginHandler(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+{
+    public async Task<ApplicationUser?> ValidateCredentialsAsync(string email, string password)
+    {
+        var user = await userManager.FindByEmailAsync(email);
+
+        if (user is null) return null;
+        
+        var result = await signInManager.CheckPasswordSignInAsync(user, password, lockoutOnFailure: false);
+        
+        return result.Succeeded ? user : null;
+    }
+}
