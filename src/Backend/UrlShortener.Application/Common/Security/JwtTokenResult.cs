3b9c2817b4bddcbdebef8b9d76eb7622b7abdbb5
Kaique Reis <kreis1265@gmail.com>
2025-07-23 20:52:07 -0300

    feat: implement JwtTokenResult to represent and encapsulate JWT response

---
+++ src/Backend/UrlShortener.Application/Common/Security/JwtTokenResult.cs
@@ -0,0 +1,8 @@
+﻿namespace UrlShortener.Application.Common.Security;
+
+public class JwtTokenResult
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public string RefreshToken { get; set; } = string.Empty;
+}
