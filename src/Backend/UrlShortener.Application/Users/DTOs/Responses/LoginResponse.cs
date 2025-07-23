3af511acb6b979d86de73ef88b5d4bdad72086c2
Kaique Reis <kreis1265@gmail.com>
2025-07-23 14:56:42 -0300

    feat: add LoginResponse DTO to encapsulate user login return data

---
+++ src/Backend/UrlShortener.Application/Users/DTOs/Responses/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿using UrlShortener.Domain.Enums;
+
+namespace UrlShortener.Application.Users.DTOs.Responses;
+
+public class LoginResponse
+{
+    public string Email { get; set; } = string.Empty;
+    public UserPlan Plan { get; set; } = UserPlan.Free;
+    public string Token { get; set; } = string.Empty;
+    public DateTime TokenExpiry { get; set; }
+}
