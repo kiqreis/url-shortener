4053bd0c65ac5a9ee0528aa797b1a681960335e5
Kaique Reis <kreis1265@gmail.com>
2025-07-24 01:00:17 -0300

    feat: add RefreshTokenRequest with properties for secure token renewal flow

---
+++ src/Backend/UrlShortener.Application/Users/DTOs/Requests/RefreshTokenRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortener.Application.Users.DTOs.Requests;
+
+public class RefreshTokenRequest
+{
+    public string Token { get; set; } = string.Empty;
+    public int? NewExpiryInMinutes { get; set; }
+}
