a9af7b943dfdecc6ba2a07bfcc8c78b2cbbfa0d3
Kaique Reis <kreis1265@gmail.com>
2025-07-24 01:33:34 -0300

    feat: add user login route with JWT token response on successful authentication

---
+++ src/Backend/UrlShortener.Api/Routes/User/LoginUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Api.Common.Api;
+using UrlShortener.Application.Users.DTOs.Requests;
+using UrlShortener.Application.Users.Services;
+
+namespace UrlShortener.Api.Routes.User;
+
+public class LoginUser : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder routeBuilder) => routeBuilder.MapPost("/login", HandleAsync)
+        .WithName("User: user login")
+        .WithDescription("Performs the user login")
+        .WithOrder(3)
+        .WithOpenApi();
+
+    private static async Task<IResult> HandleAsync([FromBody] LoginRequest request, [FromServices] IIdentityService handler)
+    {
+        var result = await handler.LoginAsync(request);
+
+        return Results.Ok(result);    
+    }
+}
