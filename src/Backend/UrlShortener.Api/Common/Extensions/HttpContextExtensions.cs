0deeadde58c9277c0f342d707638cea9d9d5104e
Kaique Reis <kreis1265@gmail.com>
2025-07-29 00:32:24 -0300

    feat: add HttpContextExtensions for easier access to auth context

---
+++ src/Backend/UrlShortener.Api/Common/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using UrlShortener.Infrastructure.Security;
+
+namespace UrlShortener.Api.Common.Extensions;
+
+public static class HttpContextExtensions
+{
+    public static void SetAuthToken(this HttpContext httpContext, string token)
+    {
+        var cookieService = httpContext.RequestServices.GetRequiredService<CookieAuthService>();
+
+        cookieService.SetAuthCookie(httpContext, token);
+    }
+
+    public static void ClearAuthToken(this HttpContext httpContext)
+    {
+        var cookieService = httpContext.RequestServices.GetRequiredService<CookieAuthService>();
+
+        cookieService.RemoveAuthCookie(httpContext);
+    }
+}
