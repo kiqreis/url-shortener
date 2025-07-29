f1795fc1e5fefe43647d7db354ae964895a46f7a
Kaique Reis <kreis1265@gmail.com>
2025-07-29 00:31:36 -0300

    feat: create CookieAuthService for extracting and managing JWT from cookies

---
+++ src/Backend/UrlShortener.Infrastructure/Security/CookieAuthService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace UrlShortener.Infrastructure.Security;
+
+public class CookieAuthService(IConfiguration configuration)
+{
+    private CookieOptions GetAuthCookieOptions() => new()
+    {
+        HttpOnly = true,
+        Secure = true,
+        Path = "/",
+        SameSite = SameSiteMode.Strict,
+        Expires = DateTimeOffset.UtcNow.AddMinutes(configuration.GetValue<int>("ExpiryInMinutes"))
+    };
+
+    public void SetAuthCookie(HttpContext httpContext, string token)
+    {
+        httpContext.Response.Cookies.Append("authToken", token, GetAuthCookieOptions());
+    }
+
+    public void RemoveAuthCookie(HttpContext httpContext)
+    {
+        httpContext.Response.Cookies.Delete("authToken", new CookieOptions
+        {
+            Path = "/",
+            Secure = true
+        });
+    }
+}
